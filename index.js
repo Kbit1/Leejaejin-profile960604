672b6ea0cc085d7133e27eaefbd3b33155a578d0
Kbit1 <cjase@naver.com>
2024-12-10 23:47:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+let main_btn = document.querySelector(".first-main")
+
+main_btn.addEventListener("mousemove",function(e){
+    let x= e.offsetX
+    let y= e.offsetY
+    console.log(x,y);
+    document.querySelector(".first-main").style="transform:rotateY(-20deg)"
+})
+
+https://www.youtube.com/watch?v=YDCCauu4lIk 3분58초
