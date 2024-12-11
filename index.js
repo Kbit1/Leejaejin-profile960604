d75c47e99678e5aef86a45f962171a9e96594da6
Kbit1 <cjase@naver.com>
2024-12-12 00:00:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// index , button 클릭시 main으로 이동 //
+document.querySelector('.first-main button').addEventListener('click', function() {
+    location.href = 'main.html';
+});
+// index , button 클릭시 main으로 이동 //
 
 let main_btn = document.querySelector(".first-main")
 
@@ -5,7 +10,14 @@ main_btn.addEventListener("mousemove",function(e){
     let x= e.offsetX
     let y= e.offsetY
     console.log(x,y);
-    document.querySelector(".first-main").style="transform:rotateY(-20deg)"
+
+    // 임시대기 어지러움 let rotateY = 1/-5 * x + 20 ; 
+    document.querySelector(".first-main").style=`transform:rotateY(${rotateY}deg)`
 })
 
-https://www.youtube.com/watch?v=YDCCauu4lIk 3분58초
+
+
+
+// https://www.youtube.com/watch?v=YDCCauu4lIk 3분58초
+
+
